5a60498b96d99eeaed3824832ceb9a6fac31ce82
John Montoya <john.montoya@comercialcard.com.co>
2023-06-03 11:33:02 -0500

    Question 01

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+for row in sys.stdin:
+  elementos = row.split(",")
+  
+  linea = elementos[2]
+  linea.replace("\t","")
+  sys.stdout.write(linea+"\n")
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+dict = {}
+for row in sys.stdin:
+  if row in dict.keys():
+    dict[row] += 1
+  else:
+    dict[row] = 1
+for i in dict.keys():
+   letra = i.strip()
+   line =  letra + "\t" + str(dict[i]) + "\n"
+   sys.stdout.write(line)
