ccdb1f0101bc400967b096684c28b176e6ceac25
John Montoya <john.montoya@comercialcard.com.co>
2023-06-03 12:16:34 -0500

    Question 05

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,24 +1,22 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
 from datetime import date
-dict =  {}
+
+def prettyformat(x):
+  if x <10:
+    x = "0"+str(x)
+  else:
+    x =  str(x)
+  return x
 
 for row in sys.stdin:
-  linea = row.strip()
-  linea = linea.split(";")
-  if linea[0] in dict.keys():
-    dict[linea[0]] +=  1
-    
-  else:
-    dict[linea[0]] = 1
-
-
-lista = [(i,dict[i]) for i in dict.keys()]
-lista.sort(key=lambda x: x[0])
-
-for tupla in lista:
+  elementos = row.strip()
+  elementos = elementos.split("   ")
   
-  line =  str(tupla[0]) + "\t" + str(tupla[1]) + "\n"
-  sys.stdout.write(line) 
+  valor = date.fromisoformat(elementos[1])
+  mes = prettyformat(valor.month)
+
+   
+  sys.stdout.write(mes+"\n")
